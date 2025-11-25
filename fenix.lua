d694c0c538b198060938f046404ba03a4d0ce702
browncur <brownchurka@gmail.com>
2025-11-25 20:32:24 +1000

    Update fenix.lua

--- fenix.lua
+++ fenix.lua
@@ -5755,84 +5755,71 @@ ElementsTable.Colorpicker = (function()
 	return Element
 end)()
 ElementsTable.Input = (function()
-	local Element = {}
-	Element.__index = Element
-	Element.__type = "Input"
-
-	function Element:New(Idx, Config)
-		assert(Config.Title, "Input - Missing Title")
-		Config.Callback = Config.Callback or function() end
-
-		local Input = {
-			Value = Config.Default or "",
-			Numeric = Config.Numeric or false,
-			Finished = Config.Finished or false,
-			Callback = Config.Callback or function(Value) end,
-			Type = "Input",
-		}
-
-		local InputFrame = Components.Element(Config.Title, Config.Description, self.Container, false)
-
-		Input.SetTitle = InputFrame.SetTitle
-		Input.SetDesc = InputFrame.SetDesc
-		Input.SetVisible = InputFrame.SetVisible
-		Input.Elements = InputFrame
-
-		local Textbox = Components.Textbox(InputFrame.Frame, true)
-		Textbox.Frame.Position = UDim2.new(1, -10, 0.5, 0)
-		Textbox.Frame.AnchorPoint = Vector2.new(1, 0.5)
-		Textbox.Frame.Size = UDim2.fromOffset(160, 30)
-		Textbox.Input.Text = Config.Default or ""
-		Textbox.Input.PlaceholderText = Config.Placeholder or ""
-
-		local Box = Textbox.Input
-
-		function Input:SetValue(Text)
-			if Config.MaxLength and #Text > Config.MaxLength then
-				Text = Text:sub(1, Config.MaxLength)
-			end
-
-			if Input.Numeric then
-				if (not tonumber(Text)) and Text:len() > 0 then
-					Text = Input.Value
-				end
-			end
-
-			Input.Value = Text
-			Box.Text = Text
-
-			Library:SafeCallback(Input.Callback, Input.Value)
-			Library:SafeCallback(Input.Changed, Input.Value)
-		end
-
-		if Input.Finished then
-			AddSignal(Box.FocusLost, function(enter)
-				if not enter then
-					return
-				end
-				Input:SetValue(Box.Text)
-			end)
-		else
-			AddSignal(Box:GetPropertyChangedSignal("Text"), function()
-				Input:SetValue(Box.Text)
-			end)
-		end
-
-		function Input:OnChanged(Func)
-			Input.Changed = Func
-			Func(Input.Value)
-		end
-
-		function Input:Destroy()
-			InputFrame:Destroy()
-			Library.Options[Idx] = nil
-		end
-
-		Library.Options[Idx] = Input
-		return Input
-	end
-
-	return Element
+    local Element = {}
+    Element.__index = Element
+    Element.__type = "Input"
+    function Element:New(Idx, Config)
+        assert(Config.Title, "Input - Missing Title")
+        Config.Callback = Config.Callback or function() end
+        local Input = {
+            Value = Config.Default or "",
+            Numeric = Config.Numeric or false,
+            Finished = Config.Finished or false,
+            Callback = Config.Callback or function(Value) end,
+            Type = "Input",
+        }
+        local InputFrame = Components.Element(Config.Title, Config.Description, self.Container, false)
+        Input.SetTitle = InputFrame.SetTitle
+        Input.SetDesc = InputFrame.SetDesc
+        function Input:SetVisible(bool)
+            InputFrame:SetVisible(bool)
+        end
+        Input.Elements = InputFrame
+        local Textbox = Components.Textbox(InputFrame.Frame, true)
+        Textbox.Frame.Position = UDim2.new(1, -10, 0.5, 0)
+        Textbox.Frame.AnchorPoint = Vector2.new(1, 0.5)
+        Textbox.Frame.Size = UDim2.fromOffset(160, 30)
+        Textbox.Input.Text = Config.Default or ""
+        Textbox.Input.PlaceholderText = Config.Placeholder or ""
+        local Box = Textbox.Input
+        function Input:SetValue(Text)
+            if Config.MaxLength and #Text > Config.MaxLength then
+                Text = Text:sub(1, Config.MaxLength)
+            end
+            if Input.Numeric then
+                if (not tonumber(Text)) and Text:len() > 0 then
+                    Text = Input.Value
+                end
+            end
+            Input.Value = Text
+            Box.Text = Text
+            Library:SafeCallback(Input.Callback, Input.Value)
+            Library:SafeCallback(Input.Changed, Input.Value)
+        end
+        if Input.Finished then
+            AddSignal(Box.FocusLost, function(enter)
+                if not enter then
+                    return
+                end
+                Input:SetValue(Box.Text)
+            end)
+        else
+            AddSignal(Box:GetPropertyChangedSignal("Text"), function()
+                Input:SetValue(Box.Text)
+            end)
+        end
+        function Input:OnChanged(Func)
+            Input.Changed = Func
+            Func(Input.Value)
+        end
+        function Input:Destroy()
+            InputFrame:Destroy()
+            Library.Options[Idx] = nil
+        end
+        Library.Options[Idx] = Input
+        return Input
+    end
+    return Element
 end)()
 
 local NotificationModule = Components.Notification
